271c835cb0c1e23167906cbff2bb624443c64247
Ryan Wick <rrwick@gmail.com>
2021-07-16 10:39:10 +1000

    Add CLI options with structopt

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,43 @@
 // Public License for more details. You should have received a copy of the GNU General Public
 // License along with Polypolish. If not, see <http://www.gnu.org/licenses/>.
 
+use std::path::PathBuf;
+use structopt::StructOpt;
 
-use clap::App;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "Polypolish", about = "short-read polishing of long-read assemblies\n\
+                                          github.com/rrwick/Polypolish")]
+struct Opt {
+    #[structopt(long = "debug",
+                help = "Optional file to store per-base information for debugging purposes")]
+    debug: Option<PathBuf>,
+
+    #[structopt(short = "m", long = "max_errors", default_value = "10",
+                help = "Ignore alignments with more than this many mismatches and indels")]
+    max_errors: i32,
+
+    #[structopt(short = "d", long = "min_depth", default_value = "5",
+                help = "A base must occur at least this many times in the pileup to be considered \
+                        valid")]
+    min_depth: i32,
+
+    #[structopt(short = "f", long = "min_fraction", default_value = "0.5",
+                help = "A base must make up at least this fraction of the pileup to be considered \
+                        valid")]
+    min_fraction: f64,
+
+    #[structopt(parse(from_os_str),
+                help = "Assembly to polish (FASTA format)")]
+    assembly: PathBuf,
+
+    #[structopt(parse(from_os_str),
+                help = "Short read alignments (SAM format, one or more files)")]
+    sam: Vec<PathBuf>,
+}
 
 
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
 }
