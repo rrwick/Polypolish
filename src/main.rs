9d418ba70a14413e03c063ce30261bec769252c9
Ryan Wick <rrwick@gmail.com>
2021-11-26 08:46:26 +1100

    Fix spacing in copyright

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // Copyright 2021 Ryan Wick (rrwick@gmail.com)
 // https://github.com/rrwick/Polypolish
 
-//This file is part of Polypolish. Polypolish is free software: you can redistribute it and/or
+// This file is part of Polypolish. Polypolish is free software: you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as published by the Free Software
 // Foundation, either version 3 of the License, or (at your option) any later version. Polypolish
 // is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
